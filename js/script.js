e2261a306b9bd73fddbe9b1710c25211ade08e1c
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2023-11-01 18:15:05 -0700

    chore: comments and refactoring

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
-/***
-    Instructions 2:
-    -Focus on first text input field
-***/
 const form = document.querySelector('form');
+const otherJobInput = document.querySelector('#other-job-role');
+const jobSelect = document.querySelector("#title");
+const colorMenu = document.querySelector("#color");
+const colorLabel = colorMenu.parentElement;
+const designMenu = document.querySelector('#design');
+const totalP = document.querySelector("#activities-cost");
+const activitiesField = document.querySelector('#activities');
+const payment = document.querySelector('#payment');
+const ccDiv = document.querySelector('#credit-card');
+const paypalDiv = document.querySelector('#paypal');
+const bitcoinDiv = document.querySelector('#bitcoin');
+const allCheckboxes = document.querySelectorAll("input[type='checkbox']");
+
+
+// On page load, focus on first input field
 const nameInput = document.querySelector('input[type=text]');
 nameInput.focus();
 
-
-/***
-    Instructions 3:
-    -Initially hide other-job-role text input
-    -addEventListener to "Job Role" <select> for 'change', on 'change' display/hide the text input
-***/ 
-const otherJobInput = document.querySelector('#other-job-role');
-const jobSelect = document.querySelector("#title");
+// Hide 'Other' job text input until needed
 otherJobInput.style.display = 'none';
 
+// Hide color menu until user chooses a design
+colorMenu.style.display = 'none';
+colorLabel.style.display = 'none';
+
+// Set default payment to credit card, hide other payment info
+payment.value = 'credit-card';
+paypalDiv.style.display = 'none';
+bitcoinDiv.style.display = 'none';
+
+// If 'Other' job selected, then show relevant text input
 jobSelect.addEventListener('change', (e) => {
     if (e.target.value === 'other'){
         otherJobInput.style.display = 'block';
@@ -24,24 +38,12 @@ jobSelect.addEventListener('change', (e) => {
     }
 })
 
-
-/***
-    Instructions 4:
-    -disable color <select>
-    -set up design <select> to listen for 'change' --> when change detected --> color <select> enabled --> color <select> displays available color
-    -not: color dropdown should display only the color options associated with the selected design
-***/
-const colorMenu = document.querySelector("#color");
-const colorLabel = colorMenu.parentElement;
-const designMenu = document.querySelector('#design');
-colorMenu.style.display = 'none';
-colorLabel.style.display = 'none';
-
+// Show/hide relevant color menu for selected design
 designMenu.addEventListener('change', (e) => {
     const heartJS = colorMenu.querySelectorAll('[data-theme="heart js"]');
     const jsPuns = colorMenu.querySelectorAll('[data-theme="js puns"]');
 
-    // reset colorMenu to default state when user changes design option
+    // Reset colorMenu to default state when user changes design option
     colorMenu.querySelector('option').selected = true;
     colorMenu.style.display = 'block';
     colorLabel.style.display = 'block';
@@ -53,6 +55,7 @@ designMenu.addEventListener('change', (e) => {
         updateColorOpts(jsPuns, heartJS);
     }
 
+    // Takes design options to show/hide available colors
     function updateColorOpts(design1, design2) {
         design1.forEach(element => {
             element.style.display = 'none';
@@ -64,55 +67,26 @@ designMenu.addEventListener('change', (e) => {
 
 })
 
-/***
-    Instructions 5:
-    -Total <p> should update to reflect total cost of selected activities
-    -addEventListener to "Register for Activities" fieldset to listen for 'changes -->
-      *if activity 'checked' Total should increase by [data-cost] of the <input type='checkbox'>.
-      *if acvitity 'unchecked' Total should decrease by that amount
-    -(<p> of #activity-cost)THIS IS THE TOTAL should update to reflect adjustments made    
-***/
-
-const totalP = document.querySelector("#activities-cost");
-const activitiesField = document.querySelector('#activities');
+// Updates Total shown on page as user selects different activities
 let totalCost = 0;
-
 activitiesField.addEventListener('change', (e) => {
 
-    // if previously 'checked', then subtract the activity cost from total
+    // If previously 'checked', then subtract the activity cost from total
     if (e.target.className === 'checked'){
         totalCost -= parseInt(e.target.getAttribute('data-cost'));
         e.target.className = '';
     }
 
-    // if activity checked, then add activity cost to total and assign a 'checked' class
+    // If activity checked, then add activity cost to total and assign '.checked'
     if (e.target.checked === true){
        totalCost += parseInt(e.target.getAttribute('data-cost'));
        e.target.className = 'checked';
     } 
 
-    // update Total shown on page
+    // Update Total shown on page
     let text = `Total: $${totalCost}`;
     document.querySelector('#activities-cost').textContent = text;
 })
-
-
-/***
-    Instructions 6:
-    -CC should be default option selected
-    -CC payment section should be only section displayed
-    -when user selects different method from drop-down, form should update display to only that method
-***/
-
-const payment = document.querySelector('#payment');
-const ccDiv = document.querySelector('#credit-card');
-const paypalDiv = document.querySelector('#paypal');
-const bitcoinDiv = document.querySelector('#bitcoin');
-
-// set default payment to credit card, hide other payment info
-payment.value = 'credit-card';
-paypalDiv.style.display = 'none';
-bitcoinDiv.style.display = 'none';
 
 payment.addEventListener('change', () => {
 
@@ -124,7 +98,7 @@ payment.addEventListener('change', () => {
         updateDisplay(bitcoinDiv, paypalDiv, ccDiv);
     }
 
-    // show/hide relevant divs
+    // Show/Hide relevant payments
     function updateDisplay(showDiv, hide1, hide2){
         showDiv.style.display = 'block';
         hide1.style.display = 'none';
@@ -133,38 +107,17 @@ payment.addEventListener('change', () => {
 })
 
 
-/***
-    Instructions 7:
-    -addEventListener on form for 'submit' --> validate each required form field/section
-    -if invalid, form submission should be prevented (**use e.preventDefault()**)
-    @name cannot be blank or empty
-    @email address needs to be formatted using @ and '.'
-    @register for activities must have at least one activity selected
-    @IF AND ONLY IF cc method is selected
-        -cc must contain 13-16 digits without dashes or spaces
-        -zip code must be 5 digit number
-        -cvv must be 3 digit number
-***/
-/***
-    Instructions 9: --ON SUBMIT--
-    if invalid:
-        - add .not-valid to parent element of form field
-            -activity section: parent element is fieldset, all else label element
-        -remove .valid from parent element
-        -display .hint associated with element
-    if valid:
-        -add .valid to parent element
-        -remove .not-valid
-        -hide .hint
-***/
 
+// Form Validation on Submit
 form.addEventListener('submit', (e) => {
+    // Variables declared/initialized
     const ccMethodSelected = payment.value === 'credit-card';
     const emailInput = document.querySelector('#email');
     const ccInput = document.querySelector('#cc-num');
     const zipInput = document.querySelector('#zip');
     const cvvInput = document.querySelector('#cvv');
 
+    // Validation functions
     const registered = () => document.querySelectorAll('.checked').length >= 1;
     const isValidName = () => /^(\s)*?[A-Za-z-]+(\s)*?[A-Za-z-]*(\s)*?$/.test(nameInput.value);
     const isValidEmail = () => /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput.value);
@@ -172,6 +125,7 @@ form.addEventListener('submit', (e) => {
     const isValidZip = () => /^\d{5}$/.test(zipInput.value);
     const isValidCVV = () => /^\d{3}$/.test(cvvInput.value);
 
+    // If any invalid values exist for required fields, then prevent page refresh on submit
     if (!isValidName() ||
         !isValidEmail()||
         !isValidCC() ||
@@ -182,6 +136,7 @@ form.addEventListener('submit', (e) => {
         e.preventDefault();
     }
 
+    // 'Name' validation
     if (!isValidName()) {
         nameInput.closest('label').classList.add('not-valid');
         nameInput.closest('label').classList.remove('valid');
@@ -191,6 +146,7 @@ form.addEventListener('submit', (e) => {
         nameInput.closest('label').classList.add('valid');
     }
 
+    // 'Email' validation
     if (!isValidEmail()) {
         emailInput.closest('label').classList.remove('valid');
         emailInput.closest('label').classList.add('not-valid');
@@ -199,6 +155,7 @@ form.addEventListener('submit', (e) => {
         emailInput.closest('label').classList.add('valid');
     }
 
+    // 'Register for Activities' validation, user needs to select at least ONE
     if (!registered()) {
         document.querySelector('#activities').classList.remove('valid');
         document.querySelector('#activities').classList.add('not-valid');
@@ -207,7 +164,9 @@ form.addEventListener('submit', (e) => {
         document.querySelector('#activities').classList.add('valid');
     }
 
+    // 'Credit Card' validation, IF credit card payment method is chosen
     if (ccMethodSelected) {
+        // 'Credit Card number' validation
         if (!isValidCC()) {
             ccInput.closest('label').classList.remove('valid');
             ccInput.closest('label').classList.add('not-valid');
@@ -215,6 +174,8 @@ form.addEventListener('submit', (e) => {
             ccInput.closest('label').classList.remove('not-valid');
             ccInput.closest('label').classList.add('valid');
         }
+
+        // 'Zip code' validation
         if (!isValidZip()) {
             zipInput.closest('label').classList.remove('valid');
             zipInput.closest('label').classList.add('not-valid');
@@ -222,6 +183,8 @@ form.addEventListener('submit', (e) => {
             zipInput.closest('label').classList.remove('not-valid');
             zipInput.closest('label').classList.add('valid');
         }
+
+        // 'CVV' validation
         if (!isValidCVV()) {
             cvvInput.closest('label').classList.remove('valid');
             cvvInput.closest('label').classList.add('not-valid');
@@ -233,16 +196,7 @@ form.addEventListener('submit', (e) => {
 
 })
 
-
-/***
-    Instructions 8:
-    -addEventListener for 'checkbox' inputs for focus and blur events
-    -on focus --> add focus class to input's parent label element
-    -on blur --> remove focus class from parent lavel
-***/
-
-const allCheckboxes = document.querySelectorAll("input[type='checkbox']");
-
+// Accessibility: add focus states to checkboxes
 for (let i=0; i<allCheckboxes.length; i++) {
     allCheckboxes[i].addEventListener('focus', (e) => {
         e.target.closest('label').className = 'focus';
